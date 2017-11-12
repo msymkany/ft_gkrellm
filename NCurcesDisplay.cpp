cb61fad3d622067e67da0565680ef477f74f3963
Volodymyr Shalabai <vshalaba@e1r10p9.unit.ua>
2017-11-12 14:29:34 +0200

    1

--- NCurcesDisplay.cpp
+++ NCurcesDisplay.cpp
@@ -1,6 +1,8 @@
 #include "NCurcesDisplay.hpp"
 
-NCurcesDisplay::NCurcesDisplay() { return; }
+NCurcesDisplay::NCurcesDisplay() { 
+	initDisplay();
+	return; }
 NCurcesDisplay::NCurcesDisplay(const NCurcesDisplay & rhs) { *this = rhs; }
 NCurcesDisplay	&NCurcesDisplay::operator=(const NCurcesDisplay & rhs)
 {
@@ -9,4 +11,43 @@ NCurcesDisplay	&NCurcesDisplay::operator=(const NCurcesDisplay & rhs)
 
 NCurcesDisplay::~NCurcesDisplay() { return; }
 
+
+void NCurcesDisplay::initDisplay()
+{
+	ioctl(0, TIOCGWINSZ, &wt);
+	wnd = initscr();
+	cbreak();
+	noecho();
+	clear();
+	refresh();
+	keypad(wnd, true);
+	nodelay(wnd, true);
+	curs_set(0);
+	mvwprintw(wnd, wt.ws_row / 2, wt.ws_col / 2 - 16, "Graphic mode : g \t Text mode : t");
+	
+	while (1){
+		in_char = wgetch(wnd);
+		switch(in_char) {
+			case 'q':
+				endwin();
+				exit(0);
+				break;
+			case 'g':
+				clear();
+				mvwprintw(wnd, wt.ws_row / 2, wt.ws_col / 2 - 6, "To be continue.....");
+				break;
+			case 't':
+				start();
+			default:
+				break;
+		}
+		refresh();
+	}
+}
+
+void NCurcesDisplay::start()
+{
+	clear();
+	
+}
 
--- NCurcesDisplay.hpp
+++ NCurcesDisplay.hpp
@@ -1,14 +1,24 @@
 #ifndef NCURCESDISPLAY_HPP_
 # define NCURCESDISPLAY_HPP_
 
-#include <iostream>
+# include <iostream>
+# include <ncurses.h>
+# include "IMonitorDisplay.hpp"
+# include <sys/ioctl.h>
 
-class NCurcesDisplay {
+class NCurcesDisplay: public IMonitorDisplay {
 public:
 	NCurcesDisplay();
 	NCurcesDisplay(const NCurcesDisplay & rhs);
 	NCurcesDisplay &operator=(const NCurcesDisplay & rhs);
-	virtual ~NCurcesDisplay();
+	~NCurcesDisplay();
+	void initDisplay();
+	void start();
+
+private:
+	WINDOW *wnd;
+	int in_char;
+	struct winsize wt;
 
 };
 
